eee450b580414df562683673e528d69a9fb12ebe
Hashim Bilal <hashimbilal2002@gmail.com>
2021-06-04 12:12:56 +0500

    4-6-21 12:12 pm

--- GameOfLife/Game.cpp
+++ GameOfLife/Game.cpp
@@ -1,4 +1,5 @@
 #include"Game.h"
+#include<Windows.h>
 
 	const char TB = '\xCD';
 	const char LR = '\xBA'; 
@@ -91,47 +92,62 @@
 {
 	int index=0;
 	int index2=0;
-	string h_line(41,TB);
-	string v_line(1,LR);
-	string l_edge(1,TL);
-	string r_edge(1,TR);
+	int index3=0;
+	bool flag=false;
+	string h_line(42,TB);
+	string v_line(2,LR);
 	cout<<endl<<endl<<endl<<endl;
 	cout<<"\t\t\t\b\b";
-	cout<<l_edge;
-	cout<<h_line<<r_edge<<endl;
-	for(index=0;index<game.size;index++)
+	cout<<TL;
+	cout<<h_line<<TR;
+	cout<<endl<<"\t\t\t\b\b"<<LR<<h_line<<LR<<endl;
+	for(index=0,index3=0;index<game.size;index++)
 	{
 		cout<<"\t\t\t\b\b"<<v_line;
 		if((index==0)||(index==(game.size-1)))
 		{ 
-			for(index2=game.size-1;index2>=0;index2--)
+			for(index2=game.size-1;index2>=0;index2--,index3++)
 			{ 
 				out<<*(*(game.game_board+index)+index2)<<"\t"<<v_line;;
 			}
 		}
 		else
-			for(index2=0;index2<game.size;index2++)
-			{ 
+		{ 
+			for(index2=0;index2<game.size;index2++,index3++)
+			{
+				if(flag)
+				{ 
+					SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),19);
+					flag=false;
+				}
+				if((index3-1)==((game.size*game.size-1)/2))
+				{	
+					SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),41);
+					flag=true;
+				}
 				out<<*(*(game.game_board+index)+index2)<<"\t"<<v_line;
 			}
-		cout<<endl<<"\t\t\t\t\b\b";
-		for(index2=0;index2<game.size;index2++)
+		}
+		cout<<endl<<"\t\t\t\b\b";
+		for(index2=0;index2<=game.size;index2++)
 		{
 			cout<<v_line<<"\t";
 		}
 		cout<<endl<<"\t\t\t\b\b";
-		for(index2=0;index2<game.size;index2++)
+		for(index2=0;index2<=game.size;index2++)
 		{
 			cout<<v_line<<"\t";
 		}
 		cout<<endl<<"\t\t\t\b\b";
-		for(index2=0;index2<game.size;index2++)
+		for(index2=0;index2<=game.size;index2++)
 		{
 			cout<<v_line<<"\t";
 		}
+		cout<<endl<<"\t\t\t\b\b"<<LR<<h_line<<LR;
 		cout<<endl;
 		//cout<<endl<<"\t\t\t\b\b"<<v_line<<endl<<"\t\t\t\b\b"<<v_line<<h_line<<endl;
 	}
+	cout<<"\t\t\t\b\b"<<BL<<h_line<<BR;
 	return out;
 }
 //	istream operator >> (istream &,Game &);
