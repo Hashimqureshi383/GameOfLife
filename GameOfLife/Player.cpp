abb465d1c878f96c8d6a23bd9f90e7b6d15f18ec
Hashim Bilal <hashimbilal2002@gmail.com>
2021-06-08 21:52:07 +0500

    9:51 pm 8-6-2021

---
+++ GameOfLife/Player.cpp
@@ -0,0 +1,51 @@
+#include"Player.h"
+
+	Player::Player()
+{
+	name="Player";
+	helping_objects=new bool [4];
+	hurdles=new bool [5];
+}
+	Player::Player(const char name[],bool* arr,bool* arr1)
+{
+	this->name=name;
+	helping_objects=arr;
+	hurdles=arr1;
+}
+	Player::Player(const Player &player)
+{
+	name=player.name;
+	helping_objects=player.helping_objects;
+	hurdles=player.hurdles;
+}
+	void Player::set_name(const char name[])
+{
+	this->name=name;
+}
+	void Player::set_hurdles(bool* arr)
+{
+	hurdles=arr;
+}
+	void Player::set_helping_objects(bool* arr)
+{
+	helping_objects=arr;
+}
+	string Player::get_name()
+{
+	return name;
+}
+	bool* Player::get_hurdles()
+{
+	return hurdles;
+}
+	bool* Player::get_helping_objects()
+{
+	return helping_objects;
+}
+	bool Player::buy_hurdle()
+{
+}
+	bool Player::buy_helping_object();
+	bool Player::use_hurdle();
+	bool Player::use_helping_object();
+	Player::~Player();
